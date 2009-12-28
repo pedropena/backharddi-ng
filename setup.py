0cf868649ed00866655abb9d518bcc494a024c88
Pedro Peña Pérez <pedro.pena@open-phoenix.com>
2009-12-28 14:09:24 +0100

    Versión inicial
    Para pasar los tests: ./setup.py nosetests. Dependencias: python-nose, python-coverage.
    Al pasar los tests se muestra información de las líneas de código que cubren.
    Una linea de código no cubierta se considerará como un bug.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
+from setuptools import setup, find_packages
+setup(name='Backharddi NG',
+        version='0.1',
+        author=u'Pedro Peña Pérez',
+        author_email='pedro.pena@open-phoenix.com',
+        license='GPL',
+        setup_requires=['nose>=0.10'],
+        test_suite = "nose.collector",
+        packages=find_packages(),
+    )
---
+++ tests/test_ZeroconfService.py
@@ -0,0 +1,24 @@
+from backhardding.utils.ZeroconfService import ZeroconfService
+from twisted.internet import glib2reactor
+glib2reactor.install()
+    
+from twisted.internet import reactor
+    
+def test():
+    class MyService(ZeroconfService):
+        def __init__(self, name, port, stype="_http._tcp",domain="", host="", text=""):
+            ZeroconfService.__init__(self,name, port, stype, domain, host, text, self.new_service, self.remove_service)
+
+        def new_service(self, interface, protocol, name, stype, domain, flags):
+            print "Found service '%s' of type '%s' in domain '%s' on %i.%i. Flags: %s" % (name, stype, domain, interface, protocol, flags)
+            self.unpublish()
+    
+        def remove_service(self, interface, protocol, name, stype, domain, flags):
+            print "Service '%s' of type '%s' in domain '%s' on %i.%i disappeared. Flags: %s" % (name, stype, domain, interface, protocol, flags)
+            reactor.callLater(0.5,reactor.stop)
+   
+    MyService(name="TestService", port=3000)        
+    reactor.run()
+
+if __name__ == "__main__":
+	test()
